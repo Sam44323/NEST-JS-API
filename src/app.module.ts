e97c69a5d6aa2beabd580a108e1d9d42dd5293ad
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-10 21:09:50 +0530

    Added some comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,12 @@ import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule], // the importted modules for handling all the other routes
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+/* 
+This means that when that(same for all the other modules files) when the app is loaded, nest.js will create an instance based on this class and will decorate the objects based on the decorator
+*/
