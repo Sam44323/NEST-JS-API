ce51187c51ab5713c2eae4361a0f75402fe1889b
Suranjan <suranjanbiswas2002@gmail.com>
2021-04-11 14:26:08 +0530

    Folder for API KEYS created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MONGO_URI } from './utils/KEYS';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './products/products.module';
 
 @Module({
-  imports: [
-    ProductModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin-suranjan:admin-suranjan@cluster0.hzfia.mongodb.net/NESTJS?retryWrites=true&w=majority',
-    ),
-  ], // the imported modules for handling all the other routes
+  imports: [ProductModule, MongooseModule.forRoot(MONGO_URI)], // the imported modules for handling all the other routes
   controllers: [AppController],
   providers: [AppService],
 })
